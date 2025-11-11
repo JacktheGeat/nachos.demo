ca2c91c35cfe1e5ccc5147dee5be39bd258fc363
JacktheGEAT <jacklynch706@gmail.com>
2025-11-11 03:36:42 -0500

    added C files

---
+++ src/nachos/test/madman.c
@@ -0,0 +1,7 @@
+// idk what this is glauk was giggling the whole time.
+// it gets the addr of '0'
+int main() {
+    int* ptr = (int*)0;
+    printf("%d\n", &ptr);
+    return 0;
+}
---
+++ src/nachos/test/sum.c
@@ -0,0 +1,33 @@
+  #include "stdio.h"
+
+#define BUFFERSIZE = 16
+  // i dont know C, so im sorry this can't be more interesting, 
+  // but I tried and failed at allowing you to input two numbers for the sum, 
+  // so two basic numbers it is
+    int main() {
+        char aChar;
+        char bChar;
+        printf("insert value for 'a' (0-9): ");
+        readline(aChar, BUFFERSIZE);
+        int a = atoi(aChar);
+        printf("insert value for 'b' (0-9): ");
+        readline(bChar, BUFFERSIZE);
+        int b = atoi(bChar);
+        
+        printf("\na: %d\nb: %d\n", a, b);
+
+        int sum_without_carry = a ^ b; 
+        int carry = (a & b) << 1;
+
+        int full_sum = a;
+        int temp_b = b;
+        while (temp_b != 0) {
+            int current_sum = full_sum ^ temp_b;
+            int current_carry = (full_sum & temp_b) << 1;
+            full_sum = current_sum;
+            temp_b = current_carry;
+        }
+        printf("Final sum: %d\n", full_sum);
+
+        return 0;
+    }
